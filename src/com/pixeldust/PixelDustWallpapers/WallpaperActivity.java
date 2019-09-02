eb3a8006d65a5d56f506ab5fdd71cf03e993820e
spezi77 <spezi7713@gmx.net>
2019-09-02 13:19:20 +0200

    PD'fy the Paranoid Papers! Thanks to AOSPA team
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/PixelDustWallpapers/WallpaperActivity.java
+++ src/com/pixeldust/PixelDustWallpapers/WallpaperActivity.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.paranoid.ParanoidPapers;
+package com.pixeldust.PixelDustWallpapers;
 
 import android.app.Activity;
 import android.app.AlertDialog;
